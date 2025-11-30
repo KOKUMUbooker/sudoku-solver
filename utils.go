71c1e847d2fb99200db7b43a8abffafa55912da7
ekozel <rubansierick@gmail.com>
2025-11-30 12:06:25 +0300

    check conflict in rows

--- utils.go
+++ utils.go
@@ -14,7 +14,14 @@ func FindUnassignedLocation(grid [][]rune, row *int, col *int) bool {
 // If num is valid return true otherwise return false
 func NoConflicts(grid [][]rune, row int, col int, num rune) bool {
 	// Check if placement is valid along the column axis
+
 	// Check if placement is valid along the row axis
+	for k := 0; k < 9; k++ { // loop from 0 to 8
+		if grid[row][k] == num { // check if current value in 'k' equals 'num'
+			return false // if condition met return false, since it breaks sudoku rules
+		}
+	}
+
 	// Check if placement is valid along the box
 
 	return true // TODO: Only for use in testing - CHANGE LATER
