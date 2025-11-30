a73104f96eacaea460a15232d6c9f75e63e52f91
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-30 16:41:22 +0300

    Remove comments

--- sudoku.go
+++ sudoku.go
@@ -6,57 +6,48 @@ import (
 )
 
 func main() {
-	input := os.Args[1:] // evrything after program name
-
+	input := os.Args[1:]
 	grid := ConvStrToRuneGrid(input)
 	if grid == nil {
-		fmt.Println("Error") // invalid input
+		fmt.Println("Error")
 		return
 	}
 
-	for _, row := range grid { // TODO: Only for use in testing - DELETE LATER
+	for _, row := range grid {
 		fmt.Println(string(row))
 	}
 }
 
-// Returns the grid as a slice of []runes with an individual []rune's index
-// in the [][]rune slice represents it's row and an individual rune in []rune
-// represents it's value in the grid
 func ConvStrToRuneGrid(grid []string) [][]rune {
 	if len(grid) != 9 {
 		return nil
 	}
 
-	gridRune := make([][]rune, 9) // allocate 2d slice to hold the 9 rows
+	gridRune := make([][]rune, 9)
 	for i, row := range grid {
-		if len(row) != 9 { // each roe must contain 9 characters
-			return nil // otherwise not valid
+		if len(row) != 9 {
+			return nil
 		}
-		gridRune[i] = []rune(row) // convert string to rune slice
+		gridRune[i] = []rune(row)
 	}
 
-	return gridRune // if everything is valid, return the full 9x9 grid
+	return gridRune
 }
 
-// Solves sudoku by recursion & backtracking
 func SolveSudoku(grid [][]rune) bool {
 	var row, col int
 
-	// Checks for next '.' location & assigns it the values for row & col if found assigns them & returns true
-	// Otherwise returns false
-	if !(FindUnassignedLocation(grid, &row, &col)) { // Base condition
-		return true // All '.' locations got assigned
+	if !(FindUnassignedLocation(grid, &row, &col)) {
+		return true
 	}
 
 	for num := '1'; num <= '9'; num++ {
 		if NoConflicts(grid, row, col, num) {
-			grid[row][col] = num // Try the choic since it doesn't violate sudoku rules
-			// Since it doesn't violate soduku rules be optimistic that starting from the filled position
-			// onwards will hopefully yield a solved soduku
+			grid[row][col] = num
 			if SolveSudoku(grid) {
 				return true
 			}
-			grid[row][col] = '.' // Undo the choice
+			grid[row][col] = '.'
 		}
 	}
 
--- utils.go
+++ utils.go
@@ -1,9 +1,5 @@
 package main
 
-// Takes a pointer to row and col & checks in grid for the next position where
-// '.' is located & uses values it finds to update row & col values
-// If it find a slot containing '.', it assigns row & col to those values & returns true
-// else it returns false
 func FindUnassignedLocation(grid [][]rune, row *int, col *int) bool {
 	dotFound := false
 
@@ -25,21 +21,18 @@ func FindUnassignedLocation(grid [][]rune, row *int, col *int) bool {
 	return dotFound
 }
 
-// Checks whether the num argument passed against sudoku rules
-// (num to be unique per row, column & also per box)
-// If num is valid return true otherwise return false
 func NoConflicts(grid [][]rune, row int, col int, num rune) bool {
 	// Check if placement is valid along the column axis
 	for r := 0; r < 9; r++ {
-		if grid[r][col] == num { // same number appears in the column
+		if grid[r][col] == num {
 			return false
 		}
 	}
 
 	// Check if placement is valid along the row axis
-	for k := 0; k < 9; k++ { // loop from 0 to 8
-		if grid[row][k] == num { // check if current value in 'k' equals 'num'
-			return false // if condition met return false, since it breaks sudoku rules
+	for k := 0; k < 9; k++ {
+		if grid[row][k] == num {
+			return false
 		}
 	}
 
