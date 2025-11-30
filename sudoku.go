c219462631ce3feda52449844b8a00e81ada08a2
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-30 17:56:11 +0300

    Place all files in one directory

--- sudoku.go
+++ sudoku.go
@@ -13,6 +13,8 @@ func main() {
 		return
 	}
 
+	SolveSudoku(grid)
+
 	for _, row := range grid {
 		fmt.Println(string(row))
 	}
@@ -34,10 +36,64 @@ func ConvStrToRuneGrid(grid []string) [][]rune {
 	return gridRune
 }
 
+func FindUnassignedLocation(grid [][]rune, row *int, col *int) bool {
+	dotFound := false
+
+	for rowI, rowSlice := range grid {
+		for colI, r := range rowSlice {
+			if r == '.' {
+				dotFound = true
+				*row = rowI
+				*col = colI
+				break
+			}
+		}
+
+		if dotFound {
+			break
+		}
+	}
+
+	return dotFound
+}
+
+func NoConflicts(grid [][]rune, row int, col int, num rune) bool {
+	// Check if placement is valid along the column axis
+	for r := 0; r < 9; r++ {
+		if grid[r][col] == num {
+			return false
+		}
+	}
+
+	// Check if placement is valid along the row axis
+	for k := 0; k < 9; k++ {
+		if grid[row][k] == num {
+			return false
+		}
+	}
+
+	// Check if placement is valid along the box
+	boxRowStart := (row / 3) * 3
+	boxColStart := (col / 3) * 3
+
+	// Loop through the 3×3 box
+	for r := boxRowStart; r < boxRowStart+3; r++ {
+		for c := boxColStart; c < boxColStart+3; c++ {
+			if grid[r][c] == num {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
 func SolveSudoku(grid [][]rune) bool {
 	var row, col int
 
-	if !(FindUnassignedLocation(grid, &row, &col)) {
+	// Base condition
+	openSlotFound := FindUnassignedLocation(grid, &row, &col)
+	if openSlotFound {
 		return true
 	}
 
--- test/test.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func main() {
-	// Test your functions here
-}
